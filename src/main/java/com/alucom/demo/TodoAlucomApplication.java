17643bcf67252ea3349ffaf2bccefced473ab81e
Pedro Lima <tsc.pedrolima@gmail.com>
2025-06-16 15:54:07 -0300

    first commit

---
+++ src/main/java/com/alucom/demo/TodoAlucomApplication.java
@@ -0,0 +1,13 @@
+package com.alucom.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAlucomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAlucomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alucom/demo/TodoAlucomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alucom.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAlucomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
