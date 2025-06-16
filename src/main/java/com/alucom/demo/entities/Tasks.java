8a2922a2a3d2bc99917265ec94574b22317b4b49
Pedro Lima <tsc.pedrolima@gmail.com>
2025-06-16 16:13:41 -0300

    feat: add tasks entity

---
+++ src/main/java/com/alucom/demo/entities/Tasks.java
@@ -0,0 +1,21 @@
+package com.alucom.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tasks", schema = "todo_alucom")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Tasks {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+    private int priority;
+}
