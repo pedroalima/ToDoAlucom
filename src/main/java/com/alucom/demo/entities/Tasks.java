84c1e24593f85dbffd08cfa07aab7eda0543d231
Pedro Lima <tsc.pedrolima@gmail.com>
2025-06-18 11:11:25 -0300

    feat: add deploy docker config

--- src/main/java/com/alucom/demo/entities/Tasks.java
+++ src/main/java/com/alucom/demo/entities/Tasks.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "tasks", schema = "todo_alucom")
+@Table(name = "tasks")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
